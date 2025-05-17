9749cf54aaf74c2ee95c40602325558002740120
RICARDO OLIVEIRA <ricardooliveiraadvpb@gmail.com>
2025-05-17 15:42:30 -0300

    Adicionando resoluçãos dos testes 2 e 3

--- trabalho.js
+++ trabalho.js
@@ -1,18 +1,41 @@
 
 function geradorDeTagsDeIdentificacao (nome) {
-  return nome.toUpperCase()
+  let nomeTag = nome.toUpperCase()
+  return nomeTag
 }
 
-function verificarSePodeSerAdotado() {
-    
+function verificarSePodeSerAdotado(numero, porte) {
+    if (numero == 1 && porte == 'M'){
+      return true
+    } else {
+      return false
+    }
 }
 
-function calcularConsumoDeRacao() {
+function calcularConsumoDeRacao(nomeDog, qtdDias, peso) {
     
+    return 
 }
 
-function decidirTipoDeAtividadePorPorte() {
+function decidirTipoDeAtividadePorPorte(porte) {   
     
+    switch (porte) {
+      case 'pequeno': 
+        return 'brincar dentro de casa'
+        break;
+      
+      case 'médio': 
+        return 'brincar trancado no muro'
+        break;
+      
+      case 'grande': 
+        return 'passear na praça com o dono'
+        break;
+
+      default: 
+      return 'O dono decidirá a brincadeira para este tipo de porte'
+        break;
+    }
 }
 
 function buscarDadoAsync() {
