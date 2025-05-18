29f66c06ac26c7705cedce5a7b214234d40832cd
RICARDO OLIVEIRA <ricardooliveiraadvpb@gmail.com>
2025-05-18 12:38:58 -0300

    Resolvendo o teste 3

--- trabalho.js
+++ trabalho.js
@@ -1,40 +1,40 @@
-
 function geradorDeTagsDeIdentificacao (nome) {
-  let nomeTag = nome.toUpperCase()
+  let nomeTag = nome.toUpperCase()        
   return nomeTag
 }
 
-function verificarSePodeSerAdotado(numero, porte) {
-    if (numero == 1 && porte == 'M'){
-      return true
-    } else {
-      return false
-    }
+function verificarSePodeSerAdotado(numero, porte) {   
+  if (numero == 1 && porte == 'M'){                 
+    return true
+  } else {
+    return false
+  }
 }
 
-function calcularConsumoDeRacao(nomeDog, qtdDias, peso) {
-    
-    return 
+function calcularConsumoDeRacao(nomeDog, quantidadeDias, peso) {
+  const quantidadePorKilo = 4350 / peso //Regra de 3 pra descobrir a quantidade de gramas para 1 kg
+  let quantidadeDiaria = quantidadePorKilo * peso
+
+  return quantidadeDiaria
 }
 
-function decidirTipoDeAtividadePorPorte(porte) {   
-    
-    switch (porte) {
-      case 'pequeno': 
-        return 'brincar dentro de casa'
-        break;
+function decidirTipoDeAtividadePorPorte(porte) {     
+  switch (porte) {
+    case 'pequeno': 
+       return 'brincar dentro de casa'
+       break;
       
-      case 'médio': 
-        return 'brincar trancado no muro'
-        break;
+    case 'médio': 
+       return 'brincar trancado no muro'
+       break;
       
-      case 'grande': 
-        return 'passear na praça com o dono'
-        break;
+    case 'grande': 
+      return 'passear na praça com o dono'
+      break;
 
-      default: 
+    default: 
       return 'O dono decidirá a brincadeira para este tipo de porte'
-        break;
+      break;
     }
 }
 
@@ -48,4 +48,4 @@ export {
     calcularConsumoDeRacao,
     decidirTipoDeAtividadePorPorte,
     buscarDadoAsync
-  } 
+  }
